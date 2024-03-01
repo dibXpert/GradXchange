3beb8231c2afeb4e9e240431a3d94845a019dd26
Dib <93922964+dibXpert@users.noreply.github.com>
2024-03-02 00:15:20 +0800

    image

--- gradxchange/item/models.py
+++ gradxchange/item/models.py
@@ -4,6 +4,8 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://neurosoft.com/img/notfound.png")
+    
 
     def __str__(self):
         return self.item_name
