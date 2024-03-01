8cbd8dbe8da9e29cb1e616cf6dcf0849668102ea
Dib <93922964+dibXpert@users.noreply.github.com>
2024-03-01 22:34:23 +0800

    templates

--- gradxchange/item/urls.py
+++ gradxchange/item/urls.py
@@ -1,2 +1,13 @@
+from . import views
+from django.urls import path
 
+app_name='item'
+urlpatterns = [
+    #/item/ 
+    path('', views.index, name='index'),
+    path('item', views.item, name='item'),
+    #/item/1
+    path('<int:item_id>/', views.detail, name='detail'),
+
+]
 
--- gradxchange/item/views.py
+++ gradxchange/item/views.py
@@ -1,1 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+
+
+def index(request):
+    item_list =  Item.objects.all()
+    context = {
+        'item_list':item_list,
+    }
+    return render(request, 'item/index.html', context)
+
+
+def item(request):
+    return HttpResponse("This is item page")
+
+def detail(request,item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item':item,
+    }
+    return render(request, 'item/detail.html', context)
+    
